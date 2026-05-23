d6bb9a44863fd21cf56e29c748839261e1b91981
Roney Victor Santos <54716734+victorroney@users.noreply.github.com>
2026-05-23 16:53:40 -0300

    Add files via upload

---
+++ agenda.py
@@ -0,0 +1,31 @@
+import agenda_modulo
+
+print("Programa de agenda".center(40, "="))
+info = agenda_modulo.carregar_info()
+
+if info:
+    print("Seus compromissos salvos na agenda são:\n")
+
+    for data, compromisso in info.items():
+        print(f"{data} -> {compromisso}")
+
+else:
+        print("Nenhum compromisso salvo ainda!")
+
+while True:
+     escolha = input("\nAperte 0 para sair. Aperte qualquer outro botão para salvar um compromisso.")
+
+     if escolha == "0":
+          break
+     else:
+          data_agend = input("\nDigite a data do seu compromisso (Dia/Mês/Ano): ")
+          compro_agend = input("Digite o seu compromisso:")
+
+          agenda_modulo.salvar_info(data_agend, compro_agend)
+          print(f"\nCompromisso para o dia {data_agend} foi salvo com sucesso!")
+
+
+          print("Obrigado por usar o programa! ".center(45, "="))
+
+
+
